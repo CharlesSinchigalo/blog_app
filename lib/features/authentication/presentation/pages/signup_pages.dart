e4f7207d81c5c05c6ff290373afd57b3ecbd2514
CharlesSinchigal <csinchigalo0552@uta.edu.ec>
2024-05-07 19:00:52 -0500

    Creado la estrutura inical del proyecto

---
+++ lib/features/authentication/presentation/pages/signup_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignUpPAge extends StatefulWidget {
+  const SignUpPAge({super.key});
+
+  @override
+  State<SignUpPAge> createState() => _SignUpPAgeState();
+}
+
+class _SignUpPAgeState extends State<SignUpPAge> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
